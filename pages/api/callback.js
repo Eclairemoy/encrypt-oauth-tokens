fb2bfbb7a8c595fd48d883b5b4d80ec4b671b70d
Liz Moy <lizmoy@Lizs-MacBook-Pro.local>
2023-03-02 16:59:16 +0000

    adding callback

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -2,7 +2,19 @@ const axios = require('axios');
 const Evervault = require('@evervault/sdk');
 
 export default async function handler(req, res) {
- 
-   res.status(200).json({ data: '' })
-}
 
+    const evervault = new Evervault(process.env.EVERVAULT_API_KEY);
+    await evervault.enableOutboundRelay()
+    
+    const data  = {'client_id': process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID, 'client_secret': process.env.GITHUB_CLIENT_SECRET, 'code': req.query.code};
+    const headers = {'headers': {'accept': 'application/json'}} 
+
+    let accessToken = await axios.post('https://github.com/login/oauth/access_token', data, headers).then(response => { return response.data.access_token })
+
+    const emailHeaders = {'headers': {"Authorization": "Bearer " + accessToken, "accept": "application/vnd.github+json"}}
+    const email = await axios.get('https://api.github.com/user', emailHeaders).then(response => { return response.data })
+
+   const userData = {name: email.name, email: email.email, auth_token: accessToken}
+ 
+   res.status(200).json({ data: userData })
+}
